9f3e086278bafea5c004ba515a08eb2186edc95c
ydaydada@gmail.com <rekuill@hotmail.com>
2020-04-18 18:26:15 +0900

    fix prettie roules

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const session = require('express-session')
-const FileStore = require('session-file-store')(session)
-const next = require('next')
-const admin = require('firebase-admin')
+const express = require("express")
+const bodyParser = require("body-parser")
+const session = require("express-session")
+const FileStore = require("session-file-store")(session)
+const next = require("next")
+const admin = require("firebase-admin")
 
 const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
+const dev = process.env.NODE_ENV !== "production"
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
 const firebase = admin.initializeApp(
   {
-    credential: admin.credential.cert(require('./credentials/server')),
+    credential: admin.credential.cert(require("./credentials/server")),
   },
-  'server'
+  "server"
 )
 
 app.prepare().then(() => {
@@ -23,9 +23,9 @@ app.prepare().then(() => {
   server.use(bodyParser.json())
   server.use(
     session({
-      secret: 'geheimnis',
+      secret: "geheimnis",
       saveUninitialized: true,
-      store: new FileStore({ secret: 'geheimnis' }),
+      store: new FileStore({ secret: "geheimnis" }),
       resave: false,
       rolling: true,
       cookie: { maxAge: 604800000, httpOnly: true }, // week
@@ -37,31 +37,31 @@ app.prepare().then(() => {
     next()
   })
 
-  server.post('/api/login', (req, res) => {
+  server.post("/api/login", (req, res) => {
     if (!req.body) return res.sendStatus(400)
 
     const token = req.body.token
     firebase
       .auth()
       .verifyIdToken(token)
-      .then(decodedToken => {
+      .then((decodedToken) => {
         req.session.decodedToken = decodedToken
         return decodedToken
       })
-      .then(decodedToken => res.json({ status: true, decodedToken }))
-      .catch(error => res.json({ error }))
+      .then((decodedToken) => res.json({ status: true, decodedToken }))
+      .catch((error) => res.json({ error }))
   })
 
-  server.post('/api/logout', (req, res) => {
+  server.post("/api/logout", (req, res) => {
     req.session.decodedToken = null
     res.json({ status: true })
   })
 
-  server.get('*', (req, res) => {
+  server.get("*", (req, res) => {
     return handle(req, res)
   })
 
-  server.listen(port, err => {
+  server.listen(port, (err) => {
     if (err) throw err
     console.log(`> Ready on http://localhost:${port}`)
   })
