af5d3f3a35e7166a75424fb85fc15e7254c14591
ydaydada@gmail.com <rekuill@hotmail.com>
2020-04-23 00:28:48 +0900

    fix layout

--- pages/DashBoard.tsx
+++ pages/DashBoard.tsx
@@ -1,27 +1,27 @@
 import React from "react"
 import clsx from "clsx"
 import { makeStyles } from "@material-ui/core/styles"
-import CssBaseline from "@material-ui/core/CssBaseline"
-import Drawer from "@material-ui/core/Drawer"
-import Box from "@material-ui/core/Box"
-import AppBar from "@material-ui/core/AppBar"
-import Toolbar from "@material-ui/core/Toolbar"
-import List from "@material-ui/core/List"
-import Typography from "@material-ui/core/Typography"
-import Divider from "@material-ui/core/Divider"
-import IconButton from "@material-ui/core/IconButton"
-import Badge from "@material-ui/core/Badge"
-import Container from "@material-ui/core/Container"
-import Grid from "@material-ui/core/Grid"
-import Paper from "@material-ui/core/Paper"
-import Link from "@material-ui/core/Link"
+import {
+  CssBaseline,
+  Drawer,
+  Box,
+  AppBar,
+  Toolbar,
+  List,
+  Typography,
+  Divider,
+  IconButton,
+  Badge,
+  Container,
+  Grid,
+  Paper,
+  Link,
+  ListItem
+} from "@material-ui/core"
 import MenuIcon from "@material-ui/icons/Menu"
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft"
-import NotificationsIcon from "@material-ui/icons/Notifications"
-//import { mainListItems, secondaryListItems } from './listItems';
-//import Chart from './Chart';
-//import Deposits from './Deposits';
-//import Orders from './Orders';
+import ExitToAppIcon from "@material-ui/icons/ExitToApp"
+import { mainListItems, secondaryListItems } from 'components/ListItems'
 import { useRouter } from 'next/router'
 import { useCurrentUser } from 'utill/UserContext'
 
@@ -92,8 +92,8 @@ export default function Dashboard() {
             Dashboard
           </Typography>
           <IconButton color="inherit" onClick={onSignOut}>
-            <Badge badgeContent={4} color="secondary">
-              <NotificationsIcon />
+            <Badge >
+              <ExitToAppIcon />
             </Badge>
           </IconButton>
         </Toolbar>
@@ -111,23 +111,20 @@ export default function Dashboard() {
           </IconButton>
         </div>
         <Divider />
-        <List></List>
+        <List>{ mainListItems }</List>
         <Divider />
-        <List></List>
+        <List>{ secondaryListItems }</List>
       </Drawer>
       <main className={classes.content}>
         <div className={classes.appBarSpacer} />
         <Container maxWidth="lg" className={classes.container}>
           <Grid container spacing={3}>
-            {/* Chart */}
             <Grid item xs={12} md={8} lg={9}>
               <Paper className={fixedHeightPaper}></Paper>
             </Grid>
-            {/* Recent Deposits */}
             <Grid item xs={12} md={4} lg={3}>
               <Paper className={fixedHeightPaper}></Paper>
             </Grid>
-            {/* Recent Orders */}
             <Grid item xs={12}>
               <Paper className={classes.paper}></Paper>
             </Grid>
