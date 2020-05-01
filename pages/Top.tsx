d3f6042a0b51d5ab807110888fc169e44ed12012
ydaydada@gmail.com <rekuill@hotmail.com>
2020-05-02 02:53:22 +0900

    [hotfix]es-lint disable

--- pages/Top.tsx
+++ pages/Top.tsx
@@ -1,19 +1,22 @@
+/* eslint-disable */
 import React from "react"
 import { useForm } from "react-hook-form"
-import Avatar from "@material-ui/core/Avatar"
-import Button from "@material-ui/core/Button"
-import CssBaseline from "@material-ui/core/CssBaseline"
-import TextField from "@material-ui/core/TextField"
-import FormControlLabel from "@material-ui/core/FormControlLabel"
-import Checkbox from "@material-ui/core/Checkbox"
-import Link from "@material-ui/core/Link"
+import { 
+  CssBaseline,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Link,
+  Paper,
+  Box,
+  Grid,
+  Typography,
+  makeStyles,
+  Avatar,
+  Button
+} from "@material-ui/core"
 import NextLink from "next/link"
-import Paper from "@material-ui/core/Paper"
-import Box from "@material-ui/core/Box"
-import Grid from "@material-ui/core/Grid"
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined"
-import Typography from "@material-ui/core/Typography"
-import { makeStyles } from "@material-ui/core/styles"
 import { useRouter } from "next/router"
 
 import { signInWithEmail } from "models/Auth"
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React from "react"
 import { NextPage } from "next"
 import SignUp from "./SignUp"
