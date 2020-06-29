1b31a4b9456c33da5aefb2c4a5fddb276176ce83
ydaydada@gmail.com <rekuill@hotmail.com>
2020-06-29 14:58:29 +0900

    change word

--- components/AppBar/Default.tsx
+++ components/AppBar/Default.tsx
@@ -37,7 +37,7 @@ const DefaultAppBar = ({open, classes, handleDrawerOpen, onSignOut}) => {
           noWrap
           className={classes.title}
         >
-          dashboard
+          Dashboard
         </Typography>
         <IconButton color="inherit" onClick={()=>router.push('/DashBoard')}>
           <Badge>
